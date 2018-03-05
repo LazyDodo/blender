f9bca158bb2086e7e06e8e883c532c32756344ea
Gaia Clary <gaia.clary@machinimatrix.org>
2018-03-05 18:08:39 +0100

    Feature Collada: Added new Enum and Mappings
    * BC_animation_transform_type
    
      Enum that Contains all animation types which can be exported
    
    * BC_ANIMATION_NAME_FROM_TYPE
    
      Map from transform_type to the name that is used in the exported data
    
    * BC_ANIMATION_TYPE_FROM_NAME
    
      Maps to get the animation transformtype from RNA Pathes
      and from collada names (also needed for the Importer later)
    
    These maps and Enums will later replace hard coded integer numbers
    which are currently used all over the place and make it really hard
    to understand what exactly is going on.

--- source/blender/collada/AnimationExporter.cpp
+++ source/blender/collada/AnimationExporter.cpp
@@ -24,6 +24,70 @@
 #include "AnimationExporter.h"
 #include "MaterialExporter.h"
 
+std::map<BC_animation_transform_type, const char*> BC_ANIMATION_NAME_FROM_TYPE = {
+	{ BC_ANIMATION_TYPE_ROTATION_EULER, "rotation_euler" },
+	{ BC_ANIMATION_TYPE_ROTATION_QUAT, "rotation_quaternion" },
+	{ BC_ANIMATION_TYPE_SCALE, "scale" },
+	{ BC_ANIMATION_TYPE_LOCATION, "location" },
+
+	/* Materials */
+	{ BC_ANIMATION_TYPE_SPECULAR_HARDNESS, "specular_hardness" },
+	{ BC_ANIMATION_TYPE_SPECULAR_COLOR, "specular_color" },
+	{ BC_ANIMATION_TYPE_DIFFUSE_COLOR, "diffuse_color" },
+	{ BC_ANIMATION_TYPE_ALPHA, "alpha" },
+	{ BC_ANIMATION_TYPE_IOR, "ior" },
+
+	/* Lamps */
+	{ BC_ANIMATION_TYPE_COLOR, "color" },
+	{ BC_ANIMATION_TYPE_FALL_OFF_ANGLE, "fall_off_angle" },
+	{ BC_ANIMATION_TYPE_FALL_OFF_EXPONENT, "fall_off_exponent" },
+	{ BC_ANIMATION_TYPE_BLENDER_DIST, "blender/blender_dist" },
+
+	/* Cameras */
+	{ BC_ANIMATION_TYPE_XFOV, "xfov" },
+	{ BC_ANIMATION_TYPE_XMAG, "xmag" },
+	{ BC_ANIMATION_TYPE_ZFAR, "zfar" },
+	{ BC_ANIMATION_TYPE_ZNEAR, "znear" },
+
+	{ BC_ANIMATION_TYPE_UNKNOWN, "" }
+};
+
+std::map<const char*, BC_animation_transform_type> BC_ANIMATION_TYPE_FROM_NAME = {
+	{ "rotation_euler", BC_ANIMATION_TYPE_ROTATION_EULER },
+	{ "rotation_quaternion", BC_ANIMATION_TYPE_ROTATION_QUAT },
+	{ "scale", BC_ANIMATION_TYPE_SCALE },
+	{ "location", BC_ANIMATION_TYPE_LOCATION },
+
+	/* Materials */
+	{ "specular_hardness", BC_ANIMATION_TYPE_SPECULAR_HARDNESS },
+	{ "specular_color", BC_ANIMATION_TYPE_SPECULAR_COLOR },
+	{ "diffuse_color", BC_ANIMATION_TYPE_DIFFUSE_COLOR },
+	{ "alpha", BC_ANIMATION_TYPE_ALPHA },
+	{ "ior", BC_ANIMATION_TYPE_IOR },
+
+	/* Lamps */
+	{ "color", BC_ANIMATION_TYPE_COLOR },
+	{ "fall_off_angle", BC_ANIMATION_TYPE_FALL_OFF_ANGLE },
+	{ "fall_off_exponent", BC_ANIMATION_TYPE_FALL_OFF_EXPONENT },
+	{ "blender/blender_dist", BC_ANIMATION_TYPE_BLENDER_DIST },
+	/* Lamp  RNA to animation type */
+	{ "spot_size", BC_ANIMATION_TYPE_FALL_OFF_ANGLE },
+	{ "spot_blend", BC_ANIMATION_TYPE_FALL_OFF_EXPONENT },
+	{ "distance", BC_ANIMATION_TYPE_BLENDER_DIST },
+
+	/* Cameras */
+	{ "xfov", BC_ANIMATION_TYPE_XFOV },
+	{ "xmag", BC_ANIMATION_TYPE_XMAG },
+	{ "zfar", BC_ANIMATION_TYPE_ZFAR },
+	{ "znear", BC_ANIMATION_TYPE_ZNEAR },
+	/* Camera RNA to animation type */
+	{ "lens", BC_ANIMATION_TYPE_XFOV },
+	{ "ortho_scale", BC_ANIMATION_TYPE_XMAG },
+	{ "clip_end", BC_ANIMATION_TYPE_ZFAR },
+	{ "clip_start", BC_ANIMATION_TYPE_ZNEAR }
+
+};
+
 template<class Functor>
 void forEachObjectInExportSet(Scene *sce, Functor &f, LinkNode *export_set)
 {
