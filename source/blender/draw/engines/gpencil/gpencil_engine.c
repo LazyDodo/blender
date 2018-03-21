71f465b01ad56ca94ddbd2283cadd09ac5f8ed47
Antonio Vazquez <blendergit@gmail.com>
2018-03-21 17:14:11 +0100

    Use evaluated camera for render

--- source/blender/draw/engines/gpencil/gpencil_engine.c
+++ source/blender/draw/engines/gpencil/gpencil_engine.c
@@ -944,7 +944,7 @@ void GPENCIL_render_init(GPENCIL_Data *ved, RenderEngine *engine, struct Depsgra
 	}
 
 	/* Set the pers & view matrix. */
-	struct Object *camera = RE_GetCamera(engine->re);
+	struct Object *camera = DEG_get_evaluated_object(depsgraph, RE_GetCamera(engine->re));
 	float frame = BKE_scene_frame_get(scene);
 	RE_GetCameraWindow(engine->re, camera, frame, stl->storage->winmat);
 	RE_GetCameraModelMatrix(engine->re, camera, stl->storage->viewinv);
