8df4d2de02568344fffca72ef3161711519cd47d
Antonio Vazquez <blendergit@gmail.com>
2018-04-27 10:27:50 +0200

    Add notifier flag to update viewport
    Whitout this flag, the viewport is not updated when change something in the material.

--- source/blender/makesrna/intern/rna_material.c
+++ source/blender/makesrna/intern/rna_material.c
@@ -93,7 +93,7 @@ static void rna_Material_update(Main *UNUSED(bmain), Scene *UNUSED(scene), Point
 	Material *ma = ptr->id.data;
 
 	DEG_id_tag_update(&ma->id, 0);
-	WM_main_add_notifier(NC_MATERIAL | ND_SHADING | NC_GPENCIL, ma);
+	WM_main_add_notifier(NC_MATERIAL | ND_SHADING | NC_GPENCIL | ND_DATA, ma);
 }
 
 static void rna_Material_update_previews(Main *UNUSED(bmain), Scene *UNUSED(scene), PointerRNA *ptr)
