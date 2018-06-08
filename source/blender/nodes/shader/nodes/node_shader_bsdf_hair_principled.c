332b2d6206c128bd7ac2474c8b80a55a1fcc64af
L. E. Segovia <leonardo.segovia@cs.uns.edu.ar>
2018-06-08 09:51:16 +0000

    Randomization is (currently) only available for melanin

--- source/blender/nodes/shader/nodes/node_shader_bsdf_hair_principled.c
+++ source/blender/nodes/shader/nodes/node_shader_bsdf_hair_principled.c
@@ -105,7 +105,7 @@ static void node_shader_update_hair_principled(bNodeTree *UNUSED(ntree), bNode *
 			}
 		}
 		else if (STREQ(sock->name, "Color Randomization")) {
-			if ((parametrization == SHD_PRINCIPLED_HAIR_PIGMENT_CONCENTRATION) || (parametrization == SHD_PRINCIPLED_HAIR_REFLECTANCE)){
+			if (parametrization == SHD_PRINCIPLED_HAIR_PIGMENT_CONCENTRATION){
 				sock->flag &= ~SOCK_UNAVAIL;
 			}
 			else {
