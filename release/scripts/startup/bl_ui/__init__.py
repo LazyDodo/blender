9b06c23dc3e0e88a12e2b1ee498b4ffdbc40245a
Lukas Tönne <lukas.toenne@gmail.com>
2018-06-30 10:20:18 +0100

    Remove unused properties_hair.py file.

--- release/scripts/startup/bl_ui/__init__.py
+++ release/scripts/startup/bl_ui/__init__.py
@@ -42,7 +42,6 @@ _modules = [
     "properties_data_lightprobe",
     "properties_data_speaker",
     "properties_data_workspace",
-    "properties_hair",
     "properties_mask_common",
     "properties_material",
     "properties_object",
