5652abe07b2dddaf800651ae2ba96082d9da4146
L. E. Segovia <leonardo.segovia@cs.uns.edu.ar>
2018-07-08 23:57:25 +0000

    Cleanup

--- source/blender/nodes/shader/nodes/node_shader_bsdf_hair_principled.c
+++ source/blender/nodes/shader/nodes/node_shader_bsdf_hair_principled.c
@@ -42,8 +42,8 @@ static bNodeSocketTemplate sh_node_bsdf_hair_principled_in[] = {
 	{	SOCK_FLOAT,		1, N_("Undercoat Roughness"),		1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, PROP_FACTOR},
 	{	SOCK_FLOAT,		1, N_("IOR"),						1.55f, 0.0f, 0.0f, 0.0f, 0.0f, 1000.0f},
 	{	SOCK_FLOAT,		1, N_("Offset"),					2.f*((float)M_PI)/180.f, 0.0f, 0.0f, 0.0f, -M_PI_2, M_PI_2, PROP_ANGLE},
-	{	SOCK_FLOAT, 	1, N_("Color Randomization"), 		0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, PROP_FACTOR},
-	{	SOCK_FLOAT, 	1, N_("Roughness Randomization"), 	0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, PROP_FACTOR},
+	{	SOCK_FLOAT, 	1, N_("Color Randomization"),		0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, PROP_FACTOR},
+	{	SOCK_FLOAT, 	1, N_("Roughness Randomization"),	0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, PROP_FACTOR},
 	{	SOCK_FLOAT,		1, N_("Random")},
 	{	-1, 0, ""	},
 };
