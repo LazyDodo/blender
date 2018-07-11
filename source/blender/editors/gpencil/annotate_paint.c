8dc7c6bc473dd15525a8049997832af22fbadda4
Antonio Vazquez <blendergit@gmail.com>
2018-07-11 10:25:21 +0200

    Fix problem with Eraser cursor
    The blend modes were using the GPU functions but the parameters were the old GL mode.

--- source/blender/editors/gpencil/annotate_paint.c
+++ source/blender/editors/gpencil/annotate_paint.c
@@ -1484,7 +1484,7 @@ static void gpencil_draw_eraser(bContext *UNUSED(C), int x, int y, void *p_ptr)
 
 		GPU_line_smooth(true);
 		GPU_blend(true);
-		GPU_blend_set_func_separate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+		GPU_blend_set_func_separate(GPU_SRC_ALPHA, GPU_ONE_MINUS_SRC_ALPHA, GPU_ONE, GPU_ONE_MINUS_SRC_ALPHA);
 
 		immUniformColor4ub(255, 100, 100, 20);
 		imm_draw_circle_fill_2d(shdr_pos, x, y, p->radius, 40);
