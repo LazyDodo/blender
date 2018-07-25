d250cd950b7d0fcaae71759e9140e7a1b998d38b
Nick Wu <xp8110@live.com>
2018-07-25 22:49:14 +0800

    Typo fix finalColor

--- source/blender/draw/engines/lanpr/shaders/lanpr_snake_multichannel_frag.glsl
+++ source/blender/draw/engines/lanpr/shaders/lanpr_snake_multichannel_frag.glsl
@@ -1,5 +1,5 @@
 in vec3 normal;
-in vec4 finalcolor;
+in vec4 finalColor;
 
 float Interpolate(float between1,float between2,float value1,float value2,float key){
     float i = (key-between1)/(between2-between1);
@@ -12,6 +12,6 @@ void main(){
     //else if(value>=0.65 && value<0.85) value=Interpolate(0.65,0.85,0.15,0.75,value);
     //else if(value>=0.85 && value<0.95) value=0.75;
     //else if(value>=0.95) value=0.9;
-    gl_FragData[0] = vec4(finalcolor.rgb*value,1);
+    gl_FragData[0] = vec4(finalColor.rgb*value,1);
     gl_FragData[1] = vec4(normal,1);//vec4((normal+vec3(1))*0.5,1);
 }
