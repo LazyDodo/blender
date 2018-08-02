MakotoIchinose/blender-invasion

Feature Collada: Prepare for exporting all actions of a scene
bc_getSceneActions(Object *ob, bool all_actions)

This function is supposed to return the list of actions
to be exported for the given object. However the function
is not yet used. Also it needs an improvement such that only
actions which match the active object will be exported.
This is still work in progress.

source/blender/collada/collada_utils.cpp
@@ -138,16 +138,27 @@ Main *bc_get_main()
 	return G.main;
 }
 
-std::vector<bAction *> bc_getSceneActions()
+std::vector<bAction *> bc_getSceneActions(Object *ob, bool all_actions)
 {
 	std::vector<bAction *> actions;
-	Main *main = bc_get_main();
-	ID *id;
+	if (all_actions) {
+		Main *main = bc_get_main();
+		ID *id;
 
-	for (id = (ID *)main->action.first; id; id = (ID *)(id->next)) {
-		bAction *act = (bAction *)id;
-		actions.push_back(act);
+		for (id = (ID *)main->action.first; id; id = (ID *)(id->next)) {
+			bAction *act = (bAction *)id;
+			/* XXX This currently creates too many actions.
+			   TODO Need to check if the action is compatible to the given object
+			*/
+			actions.push_back(act);
+		}
 	}
+	else
+	{
+		bAction *action = bc_getSceneObjectAction(ob);
+		actions.push_back(action);
+	}
+
 	return actions;
 }
 

LazyDodo/blender

source/blender/collada/collada_utils.cpp
@@ -133,16 +133,27 @@ int bc_set_parent(Object *ob, Object *par, bContext *C, bool is_parent_space)
 	return true;
 }
 
-std::vector<bAction *> bc_getSceneActions(const bContext *C)
+std::vector<bAction *> bc_getSceneActions(const bContext *C, Object *ob, bool all_actions)
 {
 	std::vector<bAction *> actions;
-	Main *bmain = CTX_data_main(C);
-	ID *id;
+	if (all_actions) {
+		Main *bmain = CTX_data_main(C);
+		ID *id;
 
-	for (id = (ID *)bmain->action.first; id; id = (ID *)(id->next)) {
-		bAction *act = (bAction *)id;
-		actions.push_back(act);
+		for (id = (ID *)bmain->action.first; id; id = (ID *)(id->next)) {
+			bAction *act = (bAction *)id;
+			/* XXX This currently creates too many actions.
+			   TODO Need to check if the action is compatible to the given object
+			*/
+			actions.push_back(act);
+		}
 	}
+	else
+	{
+		bAction *action = bc_getSceneObjectAction(ob);
+		actions.push_back(action);
+	}
+
 	return actions;
 }
 