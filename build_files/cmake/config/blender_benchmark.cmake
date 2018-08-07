689ef9543a767a0375dea9b76e846d2e8ec9a9fd
Sergey Sharybin <sergey.vfx@gmail.com>
2018-08-07 17:12:38 +0200

    Fix typo in benchmark configuration
    Numpy is supposed to be disabled.

--- build_files/cmake/config/blender_benchmark.cmake
+++ build_files/cmake/config/blender_benchmark.cmake
@@ -1,6 +1,6 @@
 include("${CMAKE_CURRENT_LIST_DIR}/blender_lite.cmake")
 
-set(WITH_PYTHON_INSTALL_NUMPY           ON  CACHE BOOL "" FORCE)
+set(WITH_PYTHON_INSTALL_NUMPY           OFF CACHE BOOL "" FORCE)
 # Brecht says: KEEP THIS ENABLED. *SOMETHING* breaks otherwise.
 # Something == DPI. Go figure.
 set(WITH_GHOST_XDND                     ON  CACHE BOOL "" FORCE)
