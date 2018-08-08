22d8135be9161d59cb54a0c4dcca89887e0d30f7
Sergey Sharybin <sergey.vfx@gmail.com>
2018-08-08 16:25:13 +0200

    Disable system glew

--- build_files/cmake/config/blender_benchmark.cmake
+++ build_files/cmake/config/blender_benchmark.cmake
@@ -1,6 +1,7 @@
 include("${CMAKE_CURRENT_LIST_DIR}/blender_lite.cmake")
 
 set(WITH_PYTHON_INSTALL_NUMPY           OFF CACHE BOOL "" FORCE)
+set(WITH_SYSTEM_GLEW                    OFF CACHE BOOL "" FORCE)
 # Brecht says: KEEP THIS ENABLED. *SOMETHING* breaks otherwise.
 # Something == DPI. Go figure.
 set(WITH_GHOST_XDND                     ON  CACHE BOOL "" FORCE)
