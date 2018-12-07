a62728abeab055c28409424bdb3e0627c36fc098
Antonioya <blendergit@gmail.com>
2018-12-07 13:27:37 +0100

    GP: Remove curve for Lines
    Lines only have 2 points, so it's not logic.

--- source/blender/editors/gpencil/gpencil_primitive.c
+++ source/blender/editors/gpencil/gpencil_primitive.c
@@ -515,7 +515,7 @@ static void gp_primitive_update_strokes(bContext *C, tGPDprimitive *tgpi)
 
 		/* calc pressure */
 		float pressure = 1.0;
-		if (ELEM(tgpi->type, GP_STROKE_LINE, GP_STROKE_ARC, GP_STROKE_BEZIER)) {
+		if (ELEM(tgpi->type, GP_STROKE_ARC, GP_STROKE_BEZIER)) {
 			if ((gset->flag & GP_SCULPT_SETT_FLAG_PRIMITIVE_CURVE) && (gps->totpoints > 1)) {
 				/* normalize value to evaluate curve */
 				float value = (float)i / (gps->totpoints - 1);
