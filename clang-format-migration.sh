29525240a5547641622d037839c58e05ceefdeaa
Campbell Barton <ideasman42@gmail.com>
2019-01-07 17:22:16 +1100

    Initial clang-format

---
+++ clang-format-migration.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FILES=$(git ls-tree -r HEAD source/ intern/string --name-only | egrep \\.\(c\|cc\|cpp\|cxx\|h\|hh\|hpp\|hxx\|m\|mm\)$)
+
+# First expand tabs
+SCRIPT=$(cat <<EOF
+import sys
+for f in sys.stdin.read().splitlines():
+	print(f)
+	with open(f, 'r', encoding="utf-8") as fh:
+		data = fh.read()
+	data = data.expandtabs(4)
+	with open(f, 'w', encoding="utf-8") as fh:
+		fh.write(data)
+
+EOF
+)
+python -c "$SCRIPT" <<< $FILES
+
+# Run clang-format.
+
+# Limit to 2gib (some files can use many gig's of memory).
+ulimit -Sv 2000000
+xargs clang-format -verbose -i <<< $FILES
